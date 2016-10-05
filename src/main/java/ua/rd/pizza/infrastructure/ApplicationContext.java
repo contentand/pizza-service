7b89cb6d735db3a56748907a10cba0167a1690f6
Daniil Yurov <Daniil_Yurov@epam.com>
2016-10-05 12:58:21 +0300

    BeanBuilder added

--- src/main/java/ua/rd/pizza/infrastructure/ApplicationContext.java
+++ src/main/java/ua/rd/pizza/infrastructure/ApplicationContext.java
@@ -22,63 +22,80 @@ public class ApplicationContext implements Context {
 
     @Override
     public <T> T getBean(String beanName, Class<T> type) {
-        try {
-            if (beans.containsKey(beanName)) {
-                return type.cast(beans.get(beanName));
-            } else {
-                T initializedBean = getInitializedBean(beanName);
-                beans.put(beanName, initializedBean);
-                return initializedBean;
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if (beans.containsKey(beanName)) {
+            return type.cast(beans.get(beanName));
+        } else {
+            T initializedBean = getInitializedBean(beanName);
+            beans.put(beanName, initializedBean);
+            return initializedBean;
         }
     }
 
-    private <T> T getInitializedBean(String beanName) throws Exception {
+    private <T> T getInitializedBean(String beanName) {
         Class<?> type = config.getImplementation(beanName);
         if (type == null) throw new IllegalArgumentException("No Such Bean Found: " + beanName);
-        return instantiateType(type);
+        BeanBuilder<?> beanBuilder = new BeanBuilder<>(type);
+        beanBuilder.createBean();
+        return (T) beanBuilder.build();
     }
 
-    private <T> T instantiateType(Class<?> type) throws Exception {
-        if (hasNoConstructor(type)) {
-            return (T) type.newInstance();
-        } else {
-            Constructor<?> constructor = getConstructor(type);
+    private class BeanBuilder<T> {
+
+        private Class<T> type;
+        private T instance;
+
+        BeanBuilder(Class<T> type) {
+            this.type = type;
+        }
+        void createBean() {
+            try {
+                this.instance = instantiateType();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        T build() {
+            return this.instance;
+        }
+
+        private T instantiateType() throws Exception {
+            Constructor<?> constructor = getConstructor();
+            if (hasNoParameters(constructor)) {
+                return type.newInstance();
+            }
             Object[] initializedConstructorArgs = getInitializedConstructorArguments(constructor);
             return (T) constructor.newInstance(initializedConstructorArgs);
         }
-    }
 
-    private boolean hasNoConstructor(Class<?> type) {
-        return type.getConstructors().length == 0;
-    }
-
-    private Constructor<?> getConstructor(Class<?> type) {
-        Constructor<?>[] constructors = type.getConstructors();
-        if (hasMultiple(constructors)) throw new IllegalStateException("Multiple constructors");
-        return constructors[0];
-    }
-
-    private Object[] getInitializedConstructorArguments(Constructor<?> constructor) {
-        Class<?>[] types = constructor.getParameterTypes();
-        Object[] args = new Object[types.length];
-        for (int i = 0; i < types.length; i++) {
-            String name = getAdjustedName(types[i]);
-            args[i] = this.getBean(name);
+        private boolean hasNoParameters(Constructor<?> constructor) {
+            return constructor.getParameterTypes().length == 0;
         }
-        return args;
-    }
 
-    private boolean hasMultiple(Constructor<?>[] constructors) {
-        return constructors.length > 1;
-    }
+        private Constructor<?> getConstructor() {
+            Constructor<?>[] constructors = type.getConstructors();
+            if (hasMultiple(constructors)) throw new IllegalStateException("Multiple constructors");
+            return constructors[0];
+        }
 
-    private String getAdjustedName(Class<?> klazz) {
-        String name = klazz.getSimpleName();
-        char[] chars = name.toCharArray();
-        chars[0] = Character.toLowerCase(chars[0]);
-        return new String(chars);
+        private Object[] getInitializedConstructorArguments(Constructor<?> constructor) {
+            Class<?>[] types = constructor.getParameterTypes();
+            Object[] args = new Object[types.length];
+            for (int i = 0; i < types.length; i++) {
+                String name = getAdjustedName(types[i]);
+                args[i] = getBean(name);
+            }
+            return args;
+        }
+
+        private boolean hasMultiple(Constructor<?>[] constructors) {
+            return constructors.length > 1;
+        }
+
+        private String getAdjustedName(Class<?> klazz) {
+            String name = klazz.getSimpleName();
+            char[] chars = name.toCharArray();
+            chars[0] = Character.toLowerCase(chars[0]);
+            return new String(chars);
+        }
     }
 }
